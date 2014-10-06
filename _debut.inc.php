80d2d96be58cd79e405f7b27c6649cd08f6f2e06
RafiqAdil <rafiq.adil.786@gmail.com>
2014-10-06 16:26:54 +0200

    a

---
+++ _debut.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+echo '
+<!doctype html>
+<!-- TITRE ET MENUS -->
+<head>
+<title>Festival</title>
+<meta http-equiv="Content-Language" content="fr">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="css/cssGeneral.css" rel="stylesheet" type="text/css">
+</head>
+<body class="basePage">
+
+<!--  Tableau contenant le titre -->
+<table width="100%" cellpadding="0" cellspacing="0">
+   <tr> 
+      <td class="titre">Festival Folklores du Monde <br>
+      <span id="texteNiveau2" class="texteNiveau2">
+      Hébergement des groupes</span><br>
+      </td>
+   </tr>
+</table>
+
+<!--  Tableau contenant les menus -->
+<table width="80%" cellpadding="0" cellspacing="0" class="tabMenu" align="center">
+   <tr>
+      <td class="menu"><a href="index.php">Accueil</a></td>
+      <td class="menu"><a href="listeEtablissements.php">
+      Gestion établissements</a></td>
+      <td class="menu"><a href="consultationAttributions.php">
+      Attributions chambres</a></td>
+   </tr>
+</table>
+<br>
+</body>';
+
+
